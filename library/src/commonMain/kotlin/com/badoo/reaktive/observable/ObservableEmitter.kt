d2d1a24963228fa0a058fb386ee4836da3a9c983
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-03-15 00:08:49 +0000

    Added some javadocs

--- library/src/commonMain/kotlin/com/badoo/reaktive/observable/ObservableEmitter.kt
+++ library/src/commonMain/kotlin/com/badoo/reaktive/observable/ObservableEmitter.kt
@@ -1,8 +1,17 @@
 package com.badoo.reaktive.observable
 
+import com.badoo.reaktive.base.Emitter
 import com.badoo.reaktive.completable.CompletableEmitter
 
+/**
+ * Represents [Emitter] that in addition to [CompletableEmitter] can signal values
+ */
 interface ObservableEmitter<in T> : CompletableEmitter {
 
+    /**
+     * Signals values
+     *
+     * @param value the value
+     */
     fun onNext(value: T)
 }
