ab5d4bfb01eb57afcd99e33f19db3740217565f5
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-04-24 11:15:40 +0100

    Tests for ObservableByEmitter

---
+++ reaktive/src/commonTest/kotlin/com/badoo/reaktive/observable/ObservableByEmitterTest.kt
@@ -0,0 +1,179 @@
+package com.badoo.reaktive.observable
+
+import com.badoo.reaktive.testutils.TestObservableObserver
+import com.badoo.reaktive.testutils.TestObservableObserver.Event
+import com.badoo.reaktive.testutils.getOnErrorEvent
+import com.badoo.reaktive.testutils.getOnNextEvent
+import com.badoo.reaktive.testutils.hasOnNext
+import com.badoo.reaktive.testutils.isCompleted
+import com.badoo.reaktive.testutils.isError
+import com.badoo.reaktive.testutils.isOnCompleteEvent
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertSame
+import kotlin.test.assertTrue
+
+class ObservableByEmitterTest {
+
+    private lateinit var emitter: ObservableEmitter<Int>
+    private val observer = TestObservableObserver<Int>()
+
+    init {
+        observable<Int> { emitter = it }.subscribe(observer)
+    }
+
+    @Test
+    fun onSubscribe_called_WHEN_subscribe() {
+        assertEquals(1, observer.disposables.size)
+    }
+
+    @Test
+    fun emitted_same_values_and_completed_in_the_same_order() {
+        emitter.onNext(1)
+        emitter.onNext(2)
+        emitter.onNext(3)
+        emitter.onComplete()
+
+        assertEquals(4, observer.events.size)
+        assertEquals(1, observer.getOnNextEvent(0).value)
+        assertEquals(2, observer.getOnNextEvent(1).value)
+        assertEquals(3, observer.getOnNextEvent(2).value)
+        assertTrue(observer.isOnCompleteEvent(3))
+    }
+
+    @Test
+    fun emitted_same_values_and_completed_with_error_in_the_same_order() {
+        val error = Throwable()
+
+        emitter.onNext(1)
+        emitter.onNext(2)
+        emitter.onNext(3)
+        emitter.onError(error)
+
+        assertEquals(4, observer.events.size)
+        assertEquals(1, observer.getOnNextEvent(0).value)
+        assertEquals(2, observer.getOnNextEvent(1).value)
+        assertEquals(3, observer.getOnNextEvent(2).value)
+        assertSame(error, observer.getOnErrorEvent(3).error)
+    }
+
+    @Test
+    fun emitted_same_values_in_the_same_order_WHEN_disposed_after_producing_values() {
+        emitter.onNext(1)
+        emitter.onNext(2)
+        emitter.onNext(3)
+        observer.disposables[0].dispose()
+
+        assertEquals(1, observer.getOnNextEvent(0).value)
+        assertEquals(2, observer.getOnNextEvent(1).value)
+        assertEquals(3, observer.getOnNextEvent(2).value)
+    }
+
+    @Test
+    fun completed_WHEN_onComplete_signalled() {
+        emitter.onComplete()
+
+        assertTrue(observer.isCompleted)
+    }
+
+    @Test
+    fun completed_with_error_WHEN_onError_signalled() {
+        val error = Throwable()
+
+        emitter.onError(error)
+
+        assertTrue(observer.isError)
+        assertSame(error, observer.getOnErrorEvent(0).error)
+    }
+
+    @Test
+    fun onNext_ignored_AFTER_onCompleted_signalled() {
+        emitter.onNext(1)
+        emitter.onComplete()
+        emitter.onNext(2)
+
+        assertTrue(observer.events.last() is Event.OnComplete)
+    }
+
+    @Test
+    fun onNext_ignored_AFTER_onError_signalled() {
+        emitter.onNext(1)
+        emitter.onError(Throwable())
+        emitter.onNext(2)
+
+        assertTrue(observer.events.last() is Event.OnError)
+    }
+
+    @Test
+    fun onComplete_ignored_AFTER_onError_signalled() {
+        emitter.onNext(1)
+        emitter.onError(Throwable())
+        emitter.onComplete()
+
+        assertTrue(observer.events.last() is Event.OnError)
+    }
+
+    @Test
+    fun onError_ignored_AFTER_onCompleted_signalled() {
+        emitter.onNext(1)
+        emitter.onComplete()
+        emitter.onError(Throwable())
+
+        assertTrue(observer.events.last() is Event.OnComplete)
+    }
+
+    @Test
+    fun second_onComplete_ignored_AFTER_first_onComplete_signalled() {
+        emitter.onNext(1)
+        emitter.onComplete()
+        emitter.onComplete()
+
+        assertTrue(observer.events.last() is Event.OnComplete)
+        assertEquals(1, observer.events.count { it is Event.OnComplete })
+    }
+
+    @Test
+    fun second_onError_ignored_AFTER_first_onError_signalled() {
+        val error1 = Throwable()
+
+        emitter.onNext(1)
+        emitter.onError(error1)
+        emitter.onError(Throwable())
+
+        assertSame(error1, (observer.events.last() as Event.OnError).error)
+        assertEquals(1, observer.events.count { it is Event.OnError })
+    }
+
+    @Test
+    fun onNext_ignored_AFTER_dispose() {
+        observer.disposables[0].dispose()
+
+        emitter.onNext(1)
+
+        assertFalse(observer.hasOnNext)
+    }
+
+    @Test
+    fun disposable_disposed_AHTER_onComplete_signalled() {
+        emitter.onComplete()
+
+        assertTrue(observer.disposables[0].isDisposed)
+    }
+
+    @Test
+    fun disposable_disposed_AHTER_onError_signalled() {
+        emitter.onError(Throwable())
+
+        assertTrue(observer.disposables[0].isDisposed)
+    }
+
+    @Test
+    fun completed_with_error_WHEN_exception_during_subscribe() {
+        val error = RuntimeException()
+
+        observable<Int> { throw error }.subscribe(observer)
+
+        assertTrue(observer.isError(error))
+    }
+}
---
+++ reaktive/src/commonTest/kotlin/com/badoo/reaktive/testutils/TestObservableObserverExt.kt
@@ -0,0 +1,23 @@
+package com.badoo.reaktive.testutils
+
+import com.badoo.reaktive.testutils.TestObservableObserver.Event
+
+fun <T> TestObservableObserver<T>.getOnNextEvent(index: Int): Event.OnNext<T> =
+    events[index] as Event.OnNext<T>
+
+fun <T> TestObservableObserver<T>.isOnCompleteEvent(index: Int): Boolean =
+    events.getOrNull(index) is Event.OnComplete
+
+fun TestObservableObserver<*>.getOnErrorEvent(index: Int): Event.OnError =
+    events[index] as Event.OnError
+
+val TestObservableObserver<*>.isCompleted: Boolean
+    get() = (events.count { it is Event.OnComplete } == 1) && events.none { it is Event.OnError }
+
+val TestObservableObserver<*>.isError: Boolean
+    get() = (events.count { it is Event.OnError } == 1) && events.none { it is Event.OnComplete }
+
+val TestObservableObserver<*>.hasOnNext: Boolean get() = events.any { it is Event.OnNext }
+
+fun TestObservableObserver<*>.isError(error: Throwable): Boolean =
+    isError && events.any { (it as? Event.OnError)?.error == error }
