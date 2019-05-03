cef615960082631bfa84fb28f46978b76e9831d4
Arkadii Ivanov <arkann1985@gmail.com>
2019-05-03 17:40:43 +0100

    Fixed Single.flatten

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Flatten.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Flatten.kt
@@ -2,19 +2,15 @@ package com.badoo.reaktive.single
 
 import com.badoo.reaktive.base.ErrorCallback
 import com.badoo.reaktive.disposable.Disposable
-import com.badoo.reaktive.disposable.DisposableWrapper
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.observable
 
 fun <T> Single<Iterable<T>>.flatten(): Observable<T> =
     observable { emitter ->
-        val disposableWrapper = DisposableWrapper()
-        emitter.setDisposable(disposableWrapper)
-
         subscribeSafe(
             object : SingleObserver<Iterable<T>>, ErrorCallback by emitter {
                 override fun onSubscribe(disposable: Disposable) {
-                    disposableWrapper.set(disposable)
+                    emitter.setDisposable(disposable)
                 }
 
                 override fun onSuccess(value: Iterable<T>) {
