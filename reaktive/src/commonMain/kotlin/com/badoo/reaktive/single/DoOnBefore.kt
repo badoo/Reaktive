de933791d85565ddc12204d82ab7234f71fe02b2
Arkadii Ivanov <arkann1985@gmail.com>
2019-05-03 15:54:22 +0100

    Deliver subscribe errors to downstreams for Single

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/DoOnBefore.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/DoOnBefore.kt
@@ -1,17 +1,21 @@
 package com.badoo.reaktive.single
 
 import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.disposable.DisposableWrapper
 import com.badoo.reaktive.disposable.wrap
 import com.badoo.reaktive.utils.lock.newLock
 import com.badoo.reaktive.utils.lock.synchronized
 
 fun <T> Single<T>.doOnBeforeSubscribe(action: (Disposable) -> Unit): Single<T> =
     singleUnsafe { observer ->
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper)
+
         subscribeSafe(
             object : SingleObserver<T> by observer {
                 override fun onSubscribe(disposable: Disposable) {
+                    disposableWrapper.set(disposable)
                     action(disposable)
-                    observer.onSubscribe(disposable)
                 }
             }
         )
@@ -19,8 +23,15 @@ fun <T> Single<T>.doOnBeforeSubscribe(action: (Disposable) -> Unit): Single<T> =
 
 fun <T> Single<T>.doOnBeforeSuccess(consumer: (T) -> Unit): Single<T> =
     singleUnsafe { observer ->
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper)
+
         subscribeSafe(
             object : SingleObserver<T> by observer {
+                override fun onSubscribe(disposable: Disposable) {
+                    disposableWrapper.set(disposable)
+                }
+
                 override fun onSuccess(value: T) {
                     consumer(value)
                     observer.onSuccess(value)
@@ -31,8 +42,15 @@ fun <T> Single<T>.doOnBeforeSuccess(consumer: (T) -> Unit): Single<T> =
 
 fun <T> Single<T>.doOnBeforeError(consumer: (Throwable) -> Unit): Single<T> =
     singleUnsafe { observer ->
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper)
+
         subscribeSafe(
             object : SingleObserver<T> by observer {
+                override fun onSubscribe(disposable: Disposable) {
+                    disposableWrapper.set(disposable)
+                }
+
                 override fun onError(error: Throwable) {
                     consumer(error)
                     observer.onError(error)
@@ -43,8 +61,15 @@ fun <T> Single<T>.doOnBeforeError(consumer: (Throwable) -> Unit): Single<T> =
 
 fun <T> Single<T>.doOnBeforeTerminate(action: () -> Unit): Single<T> =
     singleUnsafe { observer ->
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper)
+
         subscribeSafe(
             object : SingleObserver<T> by observer {
+                override fun onSubscribe(disposable: Disposable) {
+                    disposableWrapper.set(disposable)
+                }
+
                 override fun onSuccess(value: T) {
                     action()
                     observer.onSuccess(value)
@@ -60,10 +85,13 @@ fun <T> Single<T>.doOnBeforeTerminate(action: () -> Unit): Single<T> =
 
 fun <T> Single<T>.doOnBeforeDispose(action: () -> Unit): Single<T> =
     singleUnsafe { observer ->
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper.wrap(onBeforeDispose = action))
+
         subscribeSafe(
             object : SingleObserver<T> by observer {
                 override fun onSubscribe(disposable: Disposable) {
-                    observer.onSubscribe(disposable.wrap(onBeforeDispose = action))
+                    disposableWrapper.set(disposable)
                 }
             }
         )
@@ -71,11 +99,29 @@ fun <T> Single<T>.doOnBeforeDispose(action: () -> Unit): Single<T> =
 
 fun <T> Single<T>.doOnBeforeFinally(action: () -> Unit): Single<T> =
     singleUnsafe { observer ->
+        val lock = newLock()
+        var isFinished = false
+
+        fun onFinally() {
+            if (isFinished) {
+                return
+            }
+
+            lock.synchronized {
+                if (isFinished) {
+                    return
+                }
+                isFinished = true
+            }
+
+            action()
+        }
+
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper.wrap(onBeforeDispose = ::onFinally))
+
         subscribeSafe(
             object : SingleObserver<T> by observer {
-                private val lock = newLock()
-                private var isFinished = false
-
                 override fun onSubscribe(disposable: Disposable) {
                     observer.onSubscribe(disposable.wrap(onBeforeDispose = ::onFinally))
                 }
@@ -88,21 +134,6 @@ fun <T> Single<T>.doOnBeforeFinally(action: () -> Unit): Single<T> =
                 override fun onError(error: Throwable) {
                     onFinally()
                     observer.onError(error)
-                }
-
-                private fun onFinally() {
-                    if (isFinished) {
-                        return
-                    }
-
-                    lock.synchronized {
-                        if (isFinished) {
-                            return
-                        }
-                        isFinished = true
-                    }
-
-                    action()
                 }
             }
         )
--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Flatten.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Flatten.kt
@@ -2,15 +2,19 @@ package com.badoo.reaktive.single
 
 import com.badoo.reaktive.base.ErrorCallback
 import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.disposable.DisposableWrapper
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.observable
 
 fun <T> Single<Iterable<T>>.flatten(): Observable<T> =
     observable { emitter ->
+        val disposableWrapper = DisposableWrapper()
+        emitter.setDisposable(disposableWrapper)
+
         subscribeSafe(
             object : SingleObserver<Iterable<T>>, ErrorCallback by emitter {
                 override fun onSubscribe(disposable: Disposable) {
-                    emitter.setDisposable(disposable)
+                    disposableWrapper.set(disposable)
                 }
 
                 override fun onSuccess(value: Iterable<T>) {
