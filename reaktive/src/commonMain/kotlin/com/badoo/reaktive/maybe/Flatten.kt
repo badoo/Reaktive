ec31bc1e6884329081e8006d0bdf0678027ce123
Arkadii Ivanov <arkann1985@gmail.com>
2019-05-03 17:34:36 +0100

    Avoid unnecessary DisposableWrappers

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Flatten.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Flatten.kt
@@ -2,19 +2,15 @@ package com.badoo.reaktive.maybe
 
 import com.badoo.reaktive.completable.CompletableCallbacks
 import com.badoo.reaktive.disposable.Disposable
-import com.badoo.reaktive.disposable.DisposableWrapper
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.observable
 
 fun <T> Maybe<Iterable<T>>.flatten(): Observable<T> =
     observable { emitter ->
-        val disposableWrapper = DisposableWrapper()
-        emitter.setDisposable(disposableWrapper)
-
         subscribeSafe(
             object : MaybeObserver<Iterable<T>>, CompletableCallbacks by emitter {
                 override fun onSubscribe(disposable: Disposable) {
-                    disposableWrapper.set(disposable)
+                    emitter.setDisposable(disposable)
                 }
 
                 override fun onSuccess(value: Iterable<T>) {
--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Transform.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Transform.kt
@@ -1,22 +1,18 @@
 package com.badoo.reaktive.maybe
 
 import com.badoo.reaktive.disposable.Disposable
-import com.badoo.reaktive.disposable.DisposableWrapper
 
 internal inline fun <T, R> Maybe<T>.transform(
     crossinline onSuccess: (value: T, onSuccess: (R) -> Unit, onComplete: () -> Unit) -> Unit
 ): Maybe<R> =
     maybe { emitter ->
-        val disposableWrapper = DisposableWrapper()
-        emitter.setDisposable(disposableWrapper)
-
         subscribeSafe(
             object : MaybeObserver<T> {
                 private val onSuccessFunction = emitter::onSuccess
                 private val onCompleteFunction = emitter::onComplete
 
                 override fun onSubscribe(disposable: Disposable) {
-                    disposableWrapper.set(disposable)
+                    emitter.setDisposable(disposable)
                 }
 
                 override fun onSuccess(value: T) {
