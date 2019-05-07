fe22863b3a4d458d615115b7faaed20ee47a584c
DannyLamarti <lamarti.danny@gmail.com>
2019-05-07 21:58:25 +0200

    Move ScanWithSeed operators to the Scan file and apply requested changes

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/Scan.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/Scan.kt
@@ -6,7 +6,6 @@ import com.badoo.reaktive.utils.Uninitialized
 import com.badoo.reaktive.utils.lock.newLock
 import com.badoo.reaktive.utils.lock.synchronized
 
-@Suppress("UNCHECKED_CAST")
 fun <T> Observable<T>.scan(accumulate: (acc: T, value: T) -> T): Observable<T> =
     observable { emitter ->
         subscribeSafe(
@@ -15,12 +14,51 @@ fun <T> Observable<T>.scan(accumulate: (acc: T, value: T) -> T): Observable<T> =
                 var cache: Any? = Uninitialized
 
                 override fun onNext(value: T) {
-                    val previous = cache
-                    val next = try {
-                        when (previous) {
-                            Uninitialized -> value
-                            else -> accumulate(previous as T, value)
+                    val previous = lock.synchronized { cache }
+                    val next =
+                        if (previous == Uninitialized) {
+                            value
+                        } else {
+                            try {
+                                @Suppress("UNCHECKED_CAST")
+                                accumulate(previous as T, value)
+                            } catch (e: Throwable) {
+                                emitter.onError(e)
+                                return
+                            }
                         }
+
+                    lock.synchronized {
+                        cache = next
+                    }
+
+                    emitter.onNext(next)
+                }
+
+                override fun onSubscribe(disposable: Disposable) =
+                    emitter.setDisposable(disposable)
+
+            }
+        )
+    }
+
+fun <T, R> Observable<T>.scan(seed: R, accumulator: (acc: R, value: T) -> R): Observable<R> =
+    scan<T, R>({ seed }, accumulator)
+
+fun <T, R> Observable<T>.scan(getSeed: () -> R, accumulate: (acc: R, value: T) -> R): Observable<R> =
+    observable { emitter ->
+        val lock = newLock()
+        var cache: R = getSeed()
+
+        emitter.onNext(cache)
+
+        subscribeSafe(
+            object : ObservableObserver<T>, CompletableCallbacks by emitter {
+
+                override fun onNext(value: T) {
+                    val previous = lock.synchronized { cache }
+                    val next = try {
+                        accumulate(previous, value)
                     } catch (e: Throwable) {
                         emitter.onError(e)
                         return
