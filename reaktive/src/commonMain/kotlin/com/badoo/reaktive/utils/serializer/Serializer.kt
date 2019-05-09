293252902ca7490e199f3a765c4d4a27415378d1
Arkadii Ivanov <arkann1985@gmail.com>
2019-05-09 20:10:32 +0100

    Sort list in-place

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/utils/serializer/Serializer.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/utils/serializer/Serializer.kt
@@ -25,7 +25,7 @@ internal abstract class Serializer<in T>(
         state
             .getAndUpdate { state ->
                 state?.copy(
-                    queue = state.queue.plus(value).let { comparator?.let(it::sortedWith) ?: it },
+                    queue = state.queue.addAndSort(value, comparator),
                     isDraining = true
                 )
             }
@@ -68,6 +68,19 @@ internal abstract class Serializer<in T>(
      * @return true if processing should continue, false otherwise
      */
     protected abstract fun onValue(value: T): Boolean
+
+    private companion object {
+        private fun <T> List<T>.addAndSort(item: T, comparator: Comparator<in T>?): List<T> {
+            val list = ArrayList<T>(size + 1)
+            list.addAll(this)
+            list.add(item)
+            if (comparator != null) {
+                list.sortWith(comparator)
+            }
+
+            return list
+        }
+    }
 
     private data class State<out T>(
         val queue: List<T> = emptyList(),
