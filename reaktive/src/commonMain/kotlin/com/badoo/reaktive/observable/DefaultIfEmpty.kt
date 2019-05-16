63da68be419d7c218ddcd9be04b2acfb0654a4b0
Pietro Caselani <pc1992@gmail.com>
2019-05-16 10:04:36 -0300

    Change DefaultIfEmpty implementation to use SwitchIfEmpty

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/DefaultIfEmpty.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/DefaultIfEmpty.kt
@@ -1,34 +1,4 @@
 package com.badoo.reaktive.observable
 
-import com.badoo.reaktive.disposable.Disposable
-import com.badoo.reaktive.disposable.DisposableWrapper
-import com.badoo.reaktive.utils.atomicreference.AtomicReference
-
 fun <T> Observable<T>.defaultIfEmpty(defaultValue: T): Observable<T> =
-    observableUnsafe { observer ->
-        val disposableWrapper = DisposableWrapper()
-        observer.onSubscribe(disposableWrapper)
-
-        subscribeSafe(
-            object : ObservableObserver<T>, ObservableCallbacks<T> by observer {
-                private val isEmpty = AtomicReference(true)
-
-                override fun onSubscribe(disposable: Disposable) {
-                    disposableWrapper.set(disposable)
-                }
-
-                override fun onNext(value: T) {
-                    isEmpty.value = false
-                    observer.onNext(value)
-                }
-
-                override fun onComplete() {
-                    if (isEmpty.value) {
-                        observer.onNext(defaultValue)
-                    }
-
-                    observer.onComplete()
-                }
-            }
-        )
-    }
+    switchIfEmpty(observableOf(defaultValue))
