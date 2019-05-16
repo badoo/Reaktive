b691880e007b476327f844373fe5ceee4f3389f7
Pietro Caselani <pc1992@gmail.com>
2019-05-16 10:02:32 -0300

    Add operator SwitchIfEmpty on Observable

---
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/SwitchIfEmpty.kt
@@ -0,0 +1,34 @@
+package com.badoo.reaktive.observable
+
+import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.disposable.DisposableWrapper
+import com.badoo.reaktive.utils.atomicreference.AtomicReference
+
+fun <T> Observable<T>.switchIfEmpty(otherObservable: Observable<T>): Observable<T> =
+    observableUnsafe { observer ->
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper)
+
+        subscribeSafe(
+            object : ObservableObserver<T>, ObservableCallbacks<T> by observer {
+                private val isEmpty = AtomicReference(true)
+
+                override fun onSubscribe(disposable: Disposable) {
+                    disposableWrapper.set(disposable)
+                }
+
+                override fun onNext(value: T) {
+                    isEmpty.value = false
+                    observer.onNext(value)
+                }
+
+                override fun onComplete() {
+                    if (isEmpty.value) {
+                        otherObservable.subscribe(this)
+                    } else {
+                        observer.onComplete()
+                    }
+                }
+            }
+        )
+    }
