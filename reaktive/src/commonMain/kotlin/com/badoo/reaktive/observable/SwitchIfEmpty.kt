90b59026c2755b5d298c4dfca3ddf069db106949
Pietro Caselani <pc1992@gmail.com>
2019-05-17 18:10:28 -0300

    Ensure we don't subscribe to same observable again using switchIfEmpty

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/SwitchIfEmpty.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/SwitchIfEmpty.kt
@@ -1,10 +1,14 @@
 package com.badoo.reaktive.observable
 
+import com.badoo.reaktive.base.ErrorCallback
 import com.badoo.reaktive.disposable.Disposable
 import com.badoo.reaktive.disposable.DisposableWrapper
 import com.badoo.reaktive.utils.atomicreference.AtomicReference
 
 fun <T> Observable<T>.switchIfEmpty(otherObservable: Observable<T>): Observable<T> =
+    switchIfEmpty { otherObservable }
+
+fun <T> Observable<T>.switchIfEmpty(otherObservable: () -> Observable<T>): Observable<T> =
     observableUnsafe { observer ->
         val disposableWrapper = DisposableWrapper()
         observer.onSubscribe(disposableWrapper)
@@ -24,7 +28,21 @@ fun <T> Observable<T>.switchIfEmpty(otherObservable: Observable<T>): Observable<
 
                 override fun onComplete() {
                     if (isEmpty.value) {
-                        otherObservable.subscribe(this)
+                        otherObservable().subscribeSafe(
+                            object : ObservableObserver<T>, ErrorCallback by this {
+                                override fun onSubscribe(disposable: Disposable) {
+                                    disposableWrapper.set(disposable)
+                                }
+
+                                override fun onNext(value: T) {
+                                    observer.onNext(value)
+                                }
+
+                                override fun onComplete() {
+                                    observer.onComplete()
+                                }
+                            }
+                        )
                     } else {
                         observer.onComplete()
                     }
--- reaktive/src/commonTest/kotlin/com/badoo/reaktive/observable/SwitchIfEmptyTest.kt
+++ reaktive/src/commonTest/kotlin/com/badoo/reaktive/observable/SwitchIfEmptyTest.kt
@@ -1,12 +1,15 @@
 package com.badoo.reaktive.observable
 
 import com.badoo.reaktive.testutils.TestObservable
+import com.badoo.reaktive.testutils.isOnCompleteEvent
 import com.badoo.reaktive.testutils.test
 import com.badoo.reaktive.testutils.values
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class SwitchIfEmptyTest: UpstreamDownstreamGenericTests by UpstreamDownstreamGenericTests<Int>({ switchIfEmpty(observableOf(10)) }) {
+class SwitchIfEmptyTest :
+    UpstreamDownstreamGenericTests by UpstreamDownstreamGenericTests<Int>({ switchIfEmpty(observableOf(10)) }) {
 
     @Test
     fun should_switch_streams_when_source_is_empty() {
@@ -43,4 +46,28 @@ class SwitchIfEmptyTest: UpstreamDownstreamGenericTests by UpstreamDownstreamGen
         assertEquals(listOf(null), observer.values)
     }
 
+    @Test
+    fun should_complete_when_both_sources_are_empty() {
+        val source = TestObservable<Int>()
+        val observer = source.switchIfEmpty(observableOfEmpty()).test()
+
+        source.onComplete()
+
+        assertEquals(1, observer.events.size)
+        assertTrue(observer.isOnCompleteEvent(0))
+        assertTrue(observer.values.isEmpty())
+    }
+
+    @Test
+    fun should_switch_streams_when_source_is_empty_using_lambda() {
+        val source = TestObservable<Int>()
+        val otherObservable = observableOf(42)
+
+        val observer = source.switchIfEmpty { otherObservable }.test()
+
+        source.onComplete()
+
+        assertEquals(2, observer.events.size)
+        assertEquals(listOf(42), observer.values)
+    }
 }
