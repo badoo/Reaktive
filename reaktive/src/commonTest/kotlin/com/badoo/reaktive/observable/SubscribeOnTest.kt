f2582fbf23694d76c067501ad1278233a298ccb6
Arkadii Ivanov <arkann1985@gmail.com>
2019-05-17 20:52:08 +0100

    Tests for Observable.subscribeOn

--- reaktive/src/commonTest/kotlin/com/badoo/reaktive/observable/SubscribeOnTest.kt
+++ reaktive/src/commonTest/kotlin/com/badoo/reaktive/observable/SubscribeOnTest.kt
@@ -3,7 +3,6 @@ package com.badoo.reaktive.observable
 import com.badoo.reaktive.testutils.TestObservable
 import com.badoo.reaktive.testutils.TestScheduler
 import com.badoo.reaktive.testutils.dispose
-import com.badoo.reaktive.testutils.hasOnNext
 import com.badoo.reaktive.testutils.hasSubscribers
 import com.badoo.reaktive.testutils.isCompleted
 import com.badoo.reaktive.testutils.isError
@@ -14,65 +13,50 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-class ObserveOnTest : UpstreamDownstreamGenericTests by UpstreamDownstreamGenericTests<Unit>({ observeOn(TestScheduler()) }) {
+class SubscribeOnTest : UpstreamDownstreamGenericTests by UpstreamDownstreamGenericTests<Unit>({ subscribeOn(TestScheduler()) }) {
 
     private val scheduler = TestScheduler(isManualProcessing = true)
     private val upstream = TestObservable<Int>()
-    private val observer = upstream.observeOn(scheduler).test()
+    private val observer = upstream.subscribeOn(scheduler).test()
 
     @Test
-    fun subscribes_synchronously() {
+    fun does_not_subscribe_synchronously() {
+        assertFalse(upstream.hasSubscribers)
+    }
+
+    @Test
+    fun subscribes_through_scheduler() {
+        scheduler.process()
+
         assertTrue(upstream.hasSubscribers)
     }
 
     @Test
-    fun does_not_emit_values_synchronously() {
+    fun emits_values_synchronously() {
+        scheduler.process()
+        observer.reset()
         upstream.onNext(0)
         upstream.onNext(1)
         upstream.onNext(2)
-
-        assertFalse(observer.hasOnNext)
-    }
-
-    @Test
-    fun emits_values_through_scheduler() {
-        upstream.onNext(0)
-        upstream.onNext(1)
-        scheduler.process()
-        upstream.onNext(2)
-        scheduler.process()
-        scheduler.process()
 
         assertEquals(listOf(0, 1, 2), observer.values)
     }
 
     @Test
-    fun does_no_complete_synchronously() {
-        upstream.onComplete()
-
-        assertFalse(observer.isCompleted)
-    }
-
-    @Test
-    fun completes_through_scheduler() {
-        upstream.onComplete()
+    fun completes_synchronously() {
         scheduler.process()
+        observer.reset()
+        upstream.onComplete()
 
         assertTrue(observer.isCompleted)
     }
 
     @Test
-    fun does_not_error_synchronously() {
-        upstream.onError(Throwable())
-
-        assertFalse(observer.isError)
-    }
-
-    @Test
-    fun errors_through_scheduler() {
-        val error = Throwable()
-        upstream.onError(error)
+    fun errors_synchronously() {
         scheduler.process()
+        val error = Throwable()
+        observer.reset()
+        upstream.onError(error)
 
         assertTrue(observer.isError(error))
     }
