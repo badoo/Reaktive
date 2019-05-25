df713877980db21d4fee3d90dc4f6ef23ba6b9e4
Arkadii Ivanov <arkann1985@gmail.com>
2019-05-25 14:15:54 +0100

    Print stack traces for JS

--- reaktive/src/jsMain/kotlin/com/badoo/reaktive/utils/ThrowableExt.kt
+++ reaktive/src/jsMain/kotlin/com/badoo/reaktive/utils/ThrowableExt.kt
@@ -1,5 +1,18 @@
 package com.badoo.reaktive.utils
 
 actual fun Throwable.printStack() {
-    // no-op
+    println(stackTrace)
+    var inner: Throwable? = cause
+    while (inner != null) {
+        println("Caused by: ${inner.stackTrace}")
+        inner = inner.cause
+    }
 }
+
+private val Throwable.stackTrace: String
+    get() =
+        try {
+            asDynamic().stack.toString()
+        } catch (ignored: Throwable) {
+            toString()
+        }
