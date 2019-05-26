fbe6aa33f93059a1bb117f697423129409eace32
Arkadii Ivanov <arkann1985@gmail.com>
2019-05-26 11:17:14 +0100

    Get rid of internal transform operator

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Map.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Map.kt
@@ -1,6 +1,17 @@
 package com.badoo.reaktive.maybe
 
+import com.badoo.reaktive.base.Observer
+import com.badoo.reaktive.base.subscribeSafe
+import com.badoo.reaktive.base.tryCatch
+import com.badoo.reaktive.completable.CompletableCallbacks
+
 fun <T, R> Maybe<T>.map(mapper: (T) -> R): Maybe<R> =
-    transform { value, onSuccess, _ ->
-        onSuccess(mapper(value))
+    maybeUnsafe { observer ->
+        subscribeSafe(
+            object : MaybeObserver<T>, Observer by observer, CompletableCallbacks by observer {
+                override fun onSuccess(value: T) {
+                    observer.tryCatch({ mapper(value) }, observer::onSuccess)
+                }
+            }
+        )
     }
--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/NotNull.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/NotNull.kt
@@ -1,8 +1,18 @@
 package com.badoo.reaktive.observable
 
+import com.badoo.reaktive.base.Observer
+import com.badoo.reaktive.base.subscribeSafe
+import com.badoo.reaktive.completable.CompletableCallbacks
+
 fun <T : Any> Observable<T?>.notNull(): Observable<T> =
-    transform { value, onNext ->
-        if (value != null) {
-            onNext(value)
-        }
+    observableUnsafe { observer ->
+        subscribeSafe(
+            object : ObservableObserver<T?>, Observer by observer, CompletableCallbacks by observer {
+                override fun onNext(value: T?) {
+                    if (value != null) {
+                        observer.onNext(value)
+                    }
+                }
+            }
+        )
     }
