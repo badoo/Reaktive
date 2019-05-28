e7343589b4cd21c9f176289f6929fa51daf1e747
Arkadii Ivanov <arkann1985@gmail.com>
2019-05-28 10:11:56 +0100

    Tests for Single observeOn and subcribeOn

---
+++ reaktive/src/commonTest/kotlin/com/badoo/reaktive/single/ObserveOnTest.kt
@@ -0,0 +1,60 @@
+package com.badoo.reaktive.single
+
+import com.badoo.reaktive.test.base.hasSubscribers
+import com.badoo.reaktive.test.scheduler.TestScheduler
+import com.badoo.reaktive.test.single.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class ObserveOnTest
+    : UpstreamDownstreamGenericTests by UpstreamDownstreamGenericTests<Unit>({ observeOn(TestScheduler()) }) {
+
+    private val scheduler = TestScheduler(isManualProcessing = true)
+    private val upstream = TestSingle<Int>()
+    private val observer = upstream.observeOn(scheduler).test()
+
+    @Test
+    fun subscribes_synchronously() {
+        assertTrue(upstream.hasSubscribers)
+    }
+
+    @Test
+    fun does_no_succeed_synchronously() {
+        upstream.onSuccess(0)
+
+        assertFalse(observer.isSuccess)
+    }
+
+    @Test
+    fun succeeds_through_scheduler() {
+        upstream.onSuccess(0)
+        scheduler.process()
+
+        assertEquals(0, observer.value)
+    }
+
+    @Test
+    fun does_not_error_synchronously() {
+        upstream.onError(Throwable())
+
+        assertFalse(observer.isError)
+    }
+
+    @Test
+    fun errors_through_scheduler() {
+        val error = Throwable()
+        upstream.onError(error)
+        scheduler.process()
+
+        assertTrue(observer.isError(error))
+    }
+
+    @Test
+    fun disposes_executor_WHEN_disposed() {
+        observer.dispose()
+
+        assertTrue(scheduler.executors.all(TestScheduler.Executor::isDisposed))
+    }
+}
