937d9990952f11f4269adc694a815525205a618d
CherryPerry <ya.cherryperry@yandex.ru>
2019-06-06 18:41:35 +0100

    Use DisposableWrapper in filter of Single and Maybe

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Filter.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Filter.kt
@@ -4,11 +4,20 @@ import com.badoo.reaktive.base.Observer
 import com.badoo.reaktive.base.subscribeSafe
 import com.badoo.reaktive.base.tryCatch
 import com.badoo.reaktive.completable.CompletableCallbacks
+import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.disposable.DisposableWrapper
 
 fun <T> Maybe<T>.filter(predicate: (T) -> Boolean): Maybe<T> =
     maybeUnsafe { observer ->
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper)
+
         subscribeSafe(
             object : MaybeObserver<T>, Observer by observer, CompletableCallbacks by observer {
+                override fun onSubscribe(disposable: Disposable) {
+                    disposableWrapper.set(disposable)
+                }
+
                 override fun onSuccess(value: T) {
                     observer.tryCatch({ predicate(value) }) {
                         if (it) {
--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Filter.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Filter.kt
@@ -4,13 +4,22 @@ import com.badoo.reaktive.base.ErrorCallback
 import com.badoo.reaktive.base.Observer
 import com.badoo.reaktive.base.subscribeSafe
 import com.badoo.reaktive.base.tryCatch
+import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.disposable.DisposableWrapper
 import com.badoo.reaktive.maybe.Maybe
 import com.badoo.reaktive.maybe.maybeUnsafe
 
 fun <T> Single<T>.filter(predicate: (T) -> Boolean): Maybe<T> =
     maybeUnsafe { observer ->
+        val disposableWrapper = DisposableWrapper()
+        observer.onSubscribe(disposableWrapper)
+
         subscribeSafe(
             object : SingleObserver<T>, Observer by observer, ErrorCallback by observer {
+                override fun onSubscribe(disposable: Disposable) {
+                    disposableWrapper.set(disposable)
+                }
+
                 override fun onSuccess(value: T) {
                     observer.tryCatch({ predicate(value) }) {
                         if (it) {
