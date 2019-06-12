46fbf7c57856ef2094dcc3496da26613e0597a00
Arkadii Ivanov <arkann1985@gmail.com>
2019-06-12 23:46:02 +0100

    Use hrtime and window respectively

--- reaktive/src/jsMain/kotlin/com/badoo/reaktive/utils/UptimeMillis.kt
+++ reaktive/src/jsMain/kotlin/com/badoo/reaktive/utils/UptimeMillis.kt
@@ -1,6 +1,16 @@
 package com.badoo.reaktive.utils
 
-import kotlin.js.Date
+import kotlin.browser.window
 
-internal actual val uptimeMillis: Long
-    get() = Date.now().toLong()
+internal actual val uptimeMillis: Long get() = if (isWindowDefined) window.performance.now().toLong() else hrTimeMillis
+
+private external val process: dynamic
+
+private val isWindowDefined: Boolean = jsTypeOf(window) != "undefined"
+
+private val hrTimeMillis: Long
+    get() {
+        val t = process.hrtime()
+
+        return ((t[0] as Int) * 1000 + (t[1] as Int) / 1000000).toLong()
+    }
