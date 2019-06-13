e5c0ac0228bda1a056556161a829b86e8195810a
CherryPerry <ya.cherryperry@yandex.ru>
2019-06-13 16:47:37 +0100

    Configure publishing into Bintray with plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,4 +11,5 @@ dependencies {
     implementation("com.moowork.gradle:gradle-node-plugin:1.3.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.31")
     implementation("com.android.tools.build:gradle:3.3.2")
+    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
 }
---
+++ buildSrc/src/main/kotlin/PublishPlugin.kt
@@ -0,0 +1,104 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+import com.jfrog.bintray.gradle.BintrayPlugin
+import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
+import org.gradle.api.publish.maven.tasks.AbstractPublishToMaven
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.findByType
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
+
+abstract class PublishPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        setupPublishingForMultiPlatform(target)
+        setupBintrayPublishingInformation(target)
+        setupBintrayTaskForKotlinMultiPlatform(target)
+    }
+
+    private fun setupPublishingForMultiPlatform(target: Project) {
+        val mppTarget = Target.currentTarget()
+        val taskConfigurationMap = mapOf(
+            "kotlinMultiplatform" to mppTarget.meta,
+            "metadata" to mppTarget.meta,
+            "jvm" to mppTarget.common,
+            JsPlugin.TARGET_NAME_JS to mppTarget.common,
+            "androidDebug" to mppTarget.common,
+            "androidRelease" to mppTarget.common,
+            "linuxX64" to mppTarget.common,
+            "linuxArm32Hfp" to mppTarget.common,
+            IosPlugin.TARGET_NAME_X32 to mppTarget.ios,
+            IosPlugin.TARGET_NAME_X64 to mppTarget.ios,
+            IosPlugin.TARGET_NAME_SIM to mppTarget.ios
+        )
+        target.extensions.findByType(KotlinMultiplatformExtension::class)?.apply {
+            targets.configureEach { configureMavenPublicationTasksForEnvironment(this, taskConfigurationMap) }
+        }
+    }
+
+    private fun configureMavenPublicationTasksForEnvironment(
+        kotlinTarget: KotlinTarget,
+        taskConfigurationMap: Map<String, Boolean>
+    ) {
+        kotlinTarget.mavenPublication {
+            kotlinTarget.project.tasks.withType(AbstractPublishToMaven::class).configureEach {
+                onlyIf {
+                    taskConfigurationMap[publication.name] == true
+                }
+            }
+        }
+    }
+
+    private fun setupBintrayPublishingInformation(target: Project) {
+        target.plugins.apply(BintrayPlugin::class)
+        target.extensions.getByType(BintrayExtension::class).apply {
+            user = target.findProperty("bintray_user")?.toString()
+            key = target.findProperty("bintray_key")?.toString()
+            pkg.apply {
+                repo = "maven"
+                name = "reaktive"
+                userOrg = "badoo"
+                vcsUrl = "https://github.com/badoo/Reaktive.git"
+                setLicenses("Apache-2.0")
+                version.name = target.property("reaktive_version")?.toString()
+            }
+        }
+    }
+
+    private fun setupBintrayTaskForKotlinMultiPlatform(target: Project) {
+        target.tasks.named("bintrayUpload", BintrayUploadTask::class) {
+            dependsOn(target.tasks.named(MavenPublishPlugin.PUBLISH_LOCAL_LIFECYCLE_TASK_NAME))
+            doFirst {
+                setPublications(
+                    project.extensions.getByType(PublishingExtension::class).publications.map {
+                        if (it is MavenPublication) {
+                            logger.warn("Uploading artifact '${it.groupId}:${it.artifactId}:${it.version}' from publication '${it.name}'")
+                        } else {
+                            logger.warn("Not maven publication?")
+                        }
+                        it.name
+                    }
+                )
+                // https://github.com/bintray/gradle-bintray-plugin/issues/229
+                this@named.publications
+                    .filterIsInstance<MavenPublication>()
+                    .forEach { publication ->
+                        val moduleFile = target.buildDir.resolve("publications/${publication.name}/module.json")
+                        if (moduleFile.exists()) {
+                            publication.artifact(object : FileBasedMavenArtifact(moduleFile) {
+                                override fun getDefaultExtension() = "module"
+                            })
+                        }
+                    }
+            }
+        }
+    }
+}
