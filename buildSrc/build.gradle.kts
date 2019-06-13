1c0b05103a9465f150140dd9bcb76c0be0ea736d
CherryPerry <ya.cherryperry@yandex.ru>
2019-06-13 17:37:17 +0100

    Remove duplicated buildscript dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 dependencies {
     implementation("com.moowork.gradle:gradle-node-plugin:1.3.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.31")
-    implementation("com.android.tools.build:gradle:3.3.2")
+    implementation("com.android.tools.build:gradle:3.4.1")
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
 }
