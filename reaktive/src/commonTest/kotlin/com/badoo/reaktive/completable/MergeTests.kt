934790a0860898a8ce3c3c2d69cf1acd266c1984
CherryPerry <ya.cherryperry@yandex.ru>
2019-06-18 19:05:40 +0100

    Test Completable.merge

---
+++ reaktive/src/commonTest/kotlin/com/badoo/reaktive/completable/MergeTests.kt
@@ -0,0 +1,67 @@
+package com.badoo.reaktive.completable
+
+import com.badoo.reaktive.test.base.hasSubscribers
+import com.badoo.reaktive.test.completable.TestCompletable
+import com.badoo.reaktive.test.completable.isComplete
+import com.badoo.reaktive.test.completable.isError
+import com.badoo.reaktive.test.completable.test
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class MergeTests : CompletableToCompletableTests by CompletableToCompletableTests({ merge(this) }) {
+
+    private val upstream1 = TestCompletable()
+    private val upstream2 = TestCompletable()
+    private val inner = merge(upstream1, upstream2).test()
+
+    @Test
+    fun subscribes_to_first_upstream_WHEN_subscribed() {
+        assertTrue(upstream1.hasSubscribers)
+    }
+
+    @Test
+    fun subscribes_to_second_upstream_WHEN_subscribed() {
+        assertTrue(upstream2.hasSubscribers)
+    }
+
+    @Test
+    fun produces_error_WHEN_second_upstream_produced_error() {
+        val throwable = Throwable()
+        upstream1.onComplete()
+        upstream2.onError(throwable)
+
+        assertTrue(inner.isError(throwable))
+    }
+
+    @Test
+    fun does_not_complete_WHEN_first_upstream_is_completed() {
+        upstream1.onComplete()
+
+        assertFalse(inner.isComplete)
+    }
+
+    @Test
+    fun does_not_complete_WHEN_second_upstream_is_completed() {
+        upstream2.onComplete()
+
+        assertFalse(inner.isComplete)
+    }
+
+    @Test
+    fun completes_WHEN_both_upstreams_are_completed() {
+        upstream1.onComplete()
+        upstream2.onComplete()
+
+        assertTrue(inner.isComplete)
+    }
+
+    @Test
+    fun disposes_both_upstreams_WHEN_disposed() {
+        inner.dispose()
+
+        assertTrue(upstream1.isDisposed)
+        assertTrue(upstream2.isDisposed)
+    }
+
+}
