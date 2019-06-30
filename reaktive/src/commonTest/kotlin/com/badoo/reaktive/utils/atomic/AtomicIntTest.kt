185811954489145d56131e1af748c1d8ea28ad59
Arkadii Ivanov <arkann1985@gmail.com>
2019-06-30 22:05:19 +0100

    Tests

---
+++ reaktive/src/commonTest/kotlin/com/badoo/reaktive/utils/atomic/AtomicIntTest.kt
@@ -0,0 +1,75 @@
+package com.badoo.reaktive.utils.atomic
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class AtomicIntTest {
+
+    @Test
+    fun default_initial_value_is_0() {
+        assertEquals(0, AtomicInt().value)
+    }
+
+    @Test
+    fun incrementAndGet_MAX_VALUE_minus_one() {
+        val ref = AtomicInt(Int.MAX_VALUE)
+        ref.incrementAndGet(-1)
+        assertEquals(Int.MAX_VALUE - 1, ref.value)
+    }
+
+    @Test
+    fun incrementAndGet_MAX_VALUE_plus_one() {
+        val ref = AtomicInt(Int.MAX_VALUE)
+        ref.incrementAndGet(1)
+        assertEquals(Int.MIN_VALUE, ref.value)
+    }
+
+
+    @Test
+    fun incrementAndGet_MIN_VALUE_minus_one() {
+        val ref = AtomicInt(Int.MIN_VALUE)
+        ref.incrementAndGet(-1)
+        assertEquals(Int.MAX_VALUE, ref.value)
+    }
+
+    @Test
+    fun incrementAndGet_MIN_VALUE_plus_one() {
+        val ref = AtomicInt(Int.MIN_VALUE)
+        ref.incrementAndGet(1)
+        assertEquals(Int.MIN_VALUE + 1, ref.value)
+    }
+
+    @Test
+    fun compareAndSet_success_from_MAX_VALUE_to_0() {
+        val ref = AtomicInt(Int.MAX_VALUE)
+        val result = ref.compareAndSet(Int.MAX_VALUE, 0)
+        assertTrue(result)
+        assertEquals(0, ref.value)
+    }
+
+    @Test
+    fun compareAndSet_fail_from_MAX_VALUE_to_0() {
+        val ref = AtomicInt(Int.MAX_VALUE)
+        val result = ref.compareAndSet(0, 0)
+        assertFalse(result)
+        assertEquals(Int.MAX_VALUE, ref.value)
+    }
+
+    @Test
+    fun compareAndSet_success_from_MIN_VALUE_to_0() {
+        val ref = AtomicInt(Int.MIN_VALUE)
+        val result = ref.compareAndSet(Int.MIN_VALUE, 0)
+        assertTrue(result)
+        assertEquals(0, ref.value)
+    }
+
+    @Test
+    fun compareAndSet_fail_from_MIN_VALUE_to_0() {
+        val ref = AtomicInt(Int.MIN_VALUE)
+        val result = ref.compareAndSet(0, 0)
+        assertFalse(result)
+        assertEquals(Int.MIN_VALUE, ref.value)
+    }
+}
---
+++ reaktive/src/commonTest/kotlin/com/badoo/reaktive/utils/atomic/AtomicLongTest.kt
@@ -0,0 +1,75 @@
+package com.badoo.reaktive.utils.atomic
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class AtomicLongTest {
+
+    @Test
+    fun default_initial_value_is_0() {
+        assertEquals(0L, AtomicLong().value)
+    }
+
+    @Test
+    fun incrementAndGet_MAX_VALUE_minus_one() {
+        val ref = AtomicLong(Long.MAX_VALUE)
+        ref.incrementAndGet(-1L)
+        assertEquals(Long.MAX_VALUE - 1L, ref.value)
+    }
+
+    @Test
+    fun incrementAndGet_MAX_VALUE_plus_one() {
+        val ref = AtomicLong(Long.MAX_VALUE)
+        ref.incrementAndGet(1L)
+        assertEquals(Long.MIN_VALUE, ref.value)
+    }
+
+
+    @Test
+    fun incrementAndGet_MIN_VALUE_minus_one() {
+        val ref = AtomicLong(Long.MIN_VALUE)
+        ref.incrementAndGet(-1L)
+        assertEquals(Long.MAX_VALUE, ref.value)
+    }
+
+    @Test
+    fun incrementAndGet_MIN_VALUE_plus_one() {
+        val ref = AtomicLong(Long.MIN_VALUE)
+        ref.incrementAndGet(1L)
+        assertEquals(Long.MIN_VALUE + 1L, ref.value)
+    }
+
+    @Test
+    fun compareAndSet_success_from_MAX_VALUE_to_0() {
+        val ref = AtomicLong(Long.MAX_VALUE)
+        val result = ref.compareAndSet(Long.MAX_VALUE, 0L)
+        assertTrue(result)
+        assertEquals(0L, ref.value)
+    }
+
+    @Test
+    fun compareAndSet_fail_from_MAX_VALUE_to_0() {
+        val ref = AtomicLong(Long.MAX_VALUE)
+        val result = ref.compareAndSet(0L, 0L)
+        assertFalse(result)
+        assertEquals(Long.MAX_VALUE, ref.value)
+    }
+
+    @Test
+    fun compareAndSet_success_from_MIN_VALUE_to_0() {
+        val ref = AtomicLong(Long.MIN_VALUE)
+        val result = ref.compareAndSet(Long.MIN_VALUE, 0L)
+        assertTrue(result)
+        assertEquals(0L, ref.value)
+    }
+
+    @Test
+    fun compareAndSet_fail_from_MIN_VALUE_to_0() {
+        val ref = AtomicLong(Long.MIN_VALUE)
+        val result = ref.compareAndSet(0L, 0L)
+        assertFalse(result)
+        assertEquals(Long.MIN_VALUE, ref.value)
+    }
+}
