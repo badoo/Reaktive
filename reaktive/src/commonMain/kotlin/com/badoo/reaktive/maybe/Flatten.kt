e3c23fc9c8c9df5ee208997d1f1f614fff1afbeb
Arkadii Ivanov <arkann1985@gmail.com>
2019-07-06 18:57:55 +0100

    Added JvmNames

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Flatten.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Flatten.kt
@@ -2,7 +2,9 @@ package com.badoo.reaktive.maybe
 
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.asObservable
+import kotlin.jvm.JvmName
 
 fun <T> Maybe<Iterable<T>>.flatten(): Observable<T> = flatMapObservable(Iterable<T>::asObservable)
 
+@JvmName("flattenObservable")
 fun <T> Maybe<Observable<T>>.flatten(): Observable<T> = flatMapObservable { it }
--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Flatten.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Flatten.kt
@@ -2,7 +2,9 @@ package com.badoo.reaktive.single
 
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.asObservable
+import kotlin.jvm.JvmName
 
 fun <T> Single<Iterable<T>>.flatten(): Observable<T> = flatMapObservable(Iterable<T>::asObservable)
 
+@JvmName("flattenObservable")
 fun <T> Single<Observable<T>>.flatten(): Observable<T> = flatMapObservable { it }
