5d1b6d0af2f953112b4e0c4a7e727dbc0f912844
Dmitry Ustimov <ustimov.d@gmail.com>
2019-08-02 00:41:40 +0300

    Added missed return types for onErrorReturn methods

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/OnErrorReturn.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/OnErrorReturn.kt
@@ -1,7 +1,7 @@
 package com.badoo.reaktive.observable
 
-fun <T> Observable<T>.onErrorReturn(valueSupplier: (Throwable) -> T) =
+fun <T> Observable<T>.onErrorReturn(valueSupplier: (Throwable) -> T): Observable<T> =
     onErrorResumeNext { throwable -> valueSupplier(throwable).toObservable() }
 
-fun <T> Observable<T>.onErrorReturnValue(value: T) =
+fun <T> Observable<T>.onErrorReturnValue(value: T): Observable<T> =
     onErrorResumeNext { value.toObservable() }
--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/OnErrorReturn.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/OnErrorReturn.kt
@@ -1,7 +1,7 @@
 package com.badoo.reaktive.single
 
-fun <T> Single<T>.onErrorReturn(valueSupplier: (Throwable) -> T) =
+fun <T> Single<T>.onErrorReturn(valueSupplier: (Throwable) -> T): Single<T> =
     onErrorResumeNext { throwable -> valueSupplier(throwable).toSingle() }
 
-fun <T> Single<T>.onErrorReturnValue(value: T) =
+fun <T> Single<T>.onErrorReturnValue(value: T): Single<T> =
     onErrorResumeNext { value.toSingle() }
