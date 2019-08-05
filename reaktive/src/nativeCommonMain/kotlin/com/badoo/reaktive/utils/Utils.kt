16e2c3ceeedec429a5cc8485b92bf922ae81ba19
Arkadii Ivanov <arkann1985@gmail.com>
2019-08-05 18:29:15 +0100

    Added comment for binarySearch result

--- reaktive/src/nativeCommonMain/kotlin/com/badoo/reaktive/utils/Utils.kt
+++ reaktive/src/nativeCommonMain/kotlin/com/badoo/reaktive/utils/Utils.kt
@@ -2,6 +2,11 @@ package com.badoo.reaktive.utils
 
 internal fun <T> List<T>.plusSorted(item: T, comparator: Comparator<in T>): List<T> =
     toCollection(ArrayList(size + 1)).apply {
-        val index = binarySearch(item, comparator).let { if (it >= 0) it else -it - 1 }
+        val index =
+            binarySearch(item, comparator).let {
+                // binarySearch returns "-insertion point - 1" if item is not found
+                if (it >= 0) it else -it - 1
+            }
+
         add(index, item)
     }
