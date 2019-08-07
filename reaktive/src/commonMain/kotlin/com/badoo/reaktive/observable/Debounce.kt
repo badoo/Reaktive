ec6d860625b6b573531fea3f201284f496a865b3
Dmitry Ustimov <ustimov.d@gmail.com>
2019-08-08 00:29:43 +0300

    Added Observable.debounce operator with selector. Added tests

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/Debounce.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/Debounce.kt
@@ -1,66 +1,7 @@
 package com.badoo.reaktive.observable
 
-import com.badoo.reaktive.base.subscribeSafe
-import com.badoo.reaktive.disposable.CompositeDisposable
-import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.completable.completableTimer
 import com.badoo.reaktive.scheduler.Scheduler
-import com.badoo.reaktive.utils.atomic.AtomicReference
-import com.badoo.reaktive.utils.atomic.update
 
 fun <T> Observable<T>.debounce(timeoutMillis: Long, scheduler: Scheduler): Observable<T> =
-    observable { emitter ->
-        val disposables = CompositeDisposable()
-        emitter.setDisposable(disposables)
-        val executor = scheduler.newExecutor()
-        disposables += executor
-
-        subscribeSafe(
-            object : ObservableObserver<T> {
-                private val pendingValue = AtomicReference<DebouncePendingValue<T>?>(null, true)
-
-                override fun onSubscribe(disposable: Disposable) {
-                    disposables += disposable
-                }
-
-                override fun onNext(value: T) {
-                    val newPendingValue = DebouncePendingValue(value)
-                    pendingValue.value = newPendingValue
-
-                    executor.cancel()
-
-                    executor.submit(timeoutMillis) {
-                        pendingValue.update {
-                            if (it === newPendingValue) null else it
-                        }
-
-                        emitter.onNext(value)
-                    }
-                }
-
-                override fun onComplete() {
-                    executor.cancel()
-
-                    executor.submit {
-                        pendingValue
-                            .value
-                            ?.value
-                            ?.also(emitter::onNext)
-
-                        emitter.onComplete()
-                    }
-                }
-
-                override fun onError(error: Throwable) {
-                    executor.cancel()
-
-                    executor.submit {
-                        emitter.onError(error)
-                    }
-                }
-            }
-        )
-    }
-
-private class DebouncePendingValue<T>(
-    val value: T
-)
+    debounce { completableTimer(timeoutMillis, scheduler) }
