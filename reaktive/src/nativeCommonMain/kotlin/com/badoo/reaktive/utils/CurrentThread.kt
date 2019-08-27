ab68c59489a650ff65237344a6481aadabc55b4e
Arkadii Ivanov <arkann1985@gmail.com>
2019-08-27 01:08:29 -0400

    Convert pthread_t to Long in a different way

--- reaktive/src/nativeCommonMain/kotlin/com/badoo/reaktive/utils/CurrentThread.kt
+++ reaktive/src/nativeCommonMain/kotlin/com/badoo/reaktive/utils/CurrentThread.kt
@@ -1,5 +1,6 @@
 package com.badoo.reaktive.utils
 
+import kotlinx.cinterop.convert
 import platform.posix.pthread_self
 
-internal actual val currentThreadId: Long get() = pthread_self().toLong()
+internal actual val currentThreadId: Long get() = pthread_self().convert()
