9b79730351ae6ebb792e7940eec081a65b4999b5
Arkadii Ivanov <arkann1985@gmail.com>
2019-08-27 15:31:31 -0400

    Rework of CurrentThread.kt for iOS

--- reaktive/src/iosCommonMain/kotlin/com/badoo/reaktive/utils/CurrentThread.kt
+++ reaktive/src/iosCommonMain/kotlin/com/badoo/reaktive/utils/CurrentThread.kt
@@ -1,8 +1,10 @@
 package com.badoo.reaktive.utils
 
 import platform.Foundation.NSThread
+import platform.posix.pthread_self
 
 internal actual val currentThreadId: Long
-    get() = pthread_self()?.pointed?.__sig?.toLong() ?: currentThreadName.hashCode().toLong()
+    get() = pthread_self()?.pointed?.__sig?.toLong() ?: NSThread.currentThread.name?.hashCode()?.toLong() ?: 0L
 
-internal actual val currentThreadName: String get() = NSThread.currentThread.name
+internal actual val currentThreadName: String
+    get() = NSThread.currentThread.name ?: pthread_self()?.pointed?.__sig?.toString() ?: "unnamed"
