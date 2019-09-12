c9a18e3f5d5274ac162845af26bcac1dcd5bd4e0
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-09-12 13:44:05 +0100

    Use launchCoroutine helper

--- coroutines-interop/src/commonMain/kotlin/com/badoo/reaktive/coroutinesinterop/FlowAsObservable.kt
+++ coroutines-interop/src/commonMain/kotlin/com/badoo/reaktive/coroutinesinterop/FlowAsObservable.kt
@@ -3,24 +3,18 @@ package com.badoo.reaktive.coroutinesinterop
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.observable
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 fun <T> Flow<T>.asObservable(context: CoroutineContext = Dispatchers.Unconfined): Observable<T> =
     observable { emitter ->
-        GlobalScope
-            .launch(context) {
-                try {
-                    collect { emitter.onNext(it) }
-                } catch (e: Throwable) {
-                    emitter.onError(e)
-                } finally {
-                    emitter.onComplete()
-                }
-            }
-            .asDisposable()
+        launchCoroutine(
+            context = context,
+            onSuccess = { emitter.onComplete() },
+            onError = emitter::onError
+        ) {
+            collect { emitter.onNext(it) }
+        }
             .also(emitter::setDisposable)
     }
--- coroutines-interop/src/commonMain/kotlin/com/badoo/reaktive/coroutinesinterop/SingleFromCoroutine.kt
+++ coroutines-interop/src/commonMain/kotlin/com/badoo/reaktive/coroutinesinterop/SingleFromCoroutine.kt
@@ -3,24 +3,16 @@ package com.badoo.reaktive.coroutinesinterop
 import com.badoo.reaktive.single.Single
 import com.badoo.reaktive.single.single
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 fun <T> singleFromCoroutine(context: CoroutineContext = Dispatchers.Unconfined, block: suspend () -> T): Single<T> =
     single { emitter ->
-        GlobalScope
-            .launch {
-                emitter.onSuccess(
-                    try {
-                        block()
-                    } catch (e: Throwable) {
-                        emitter.onError(e)
-                        return@launch
-                    }
-                )
-            }
-            .asDisposable()
+        launchCoroutine(
+            context = context,
+            onSuccess = emitter::onSuccess,
+            onError = emitter::onError,
+            block = block
+        )
             .also(emitter::setDisposable)
     }
 
