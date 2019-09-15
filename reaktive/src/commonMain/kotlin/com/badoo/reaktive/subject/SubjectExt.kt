a6525411dcc7fd491952422cfa7f7e612ce51434
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-09-15 22:53:59 +0100

    Added Observable.publish operator

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/subject/SubjectExt.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/subject/SubjectExt.kt
@@ -4,11 +4,15 @@ import com.badoo.reaktive.disposable.Disposable
 import com.badoo.reaktive.observable.ObservableCallbacks
 import com.badoo.reaktive.observable.ObservableObserver
 
-fun <T> Subject<T>.getObserver(): ObservableObserver<T> =
+fun <T> Subject<T>.getObserver(onSubscribe: ((Disposable) -> Unit)? = null): ObservableObserver<T> =
     object : ObservableObserver<T>, ObservableCallbacks<T> by this {
         override fun onSubscribe(disposable: Disposable) {
-            if (status !is Subject.Status.Active) {
+            if (isActive) {
+                onSubscribe?.invoke(disposable)
+            } else {
                 disposable.dispose()
             }
         }
     }
+
+val Subject<*>.isActive: Boolean get() = status is Subject.Status.Active
