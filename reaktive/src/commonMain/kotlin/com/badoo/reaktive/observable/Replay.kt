170b8306fa58966ec3a363d8a616a114f885ad57
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-09-30 17:02:05 +0100

    Use SharedList instead of AtomicList in replay operator

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/Replay.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/Replay.kt
@@ -2,18 +2,17 @@ package com.badoo.reaktive.observable
 
 import com.badoo.reaktive.base.operator.publish
 import com.badoo.reaktive.subject.DefaultSubject
-import com.badoo.reaktive.utils.atomic.AtomicList
-import com.badoo.reaktive.utils.atomic.plusAssign
+import com.badoo.reaktive.utils.SharedList
 
 fun <T> Observable<T>.replay(): ConnectableObservable<T> =
     publish {
         object : DefaultSubject<T>() {
-            private val values = AtomicList<T>(emptyList())
+            private val values = SharedList<T>()
 
             override fun onAfterSubscribe(observer: ObservableObserver<T>) {
                 super.onAfterSubscribe(observer)
 
-                values.value.forEach(observer::onNext)
+                values.forEach(observer::onNext)
             }
 
             override fun onBeforeNext(value: T) {
