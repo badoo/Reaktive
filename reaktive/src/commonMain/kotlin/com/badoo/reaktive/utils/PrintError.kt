db33e2d9ac5fb9964703e6bc4f71e43dc4348584
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-10-02 15:52:01 +0100

    Annotate _isPrintErrorEnabled with SharedImmutable

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/utils/PrintError.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/utils/PrintError.kt
@@ -1,8 +1,10 @@
 package com.badoo.reaktive.utils
 
 import com.badoo.reaktive.utils.atomic.AtomicBoolean
+import kotlin.native.concurrent.SharedImmutable
 
 @Suppress("ObjectPropertyName") // Backing field
+@SharedImmutable
 private val _isPrintErrorEnabled = AtomicBoolean(true)
 
 internal var isPrintErrorEnabled
