7e67ebbfb0107d4827074b0a43be3a78af3de0f9
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-10-15 10:49:05 +0100

    More readable code style

--- reaktive/src/nativeCommonMain/kotlin/com/badoo/reaktive/observable/ToList.kt
+++ reaktive/src/nativeCommonMain/kotlin/com/badoo/reaktive/observable/ToList.kt
@@ -5,7 +5,15 @@ import kotlin.native.concurrent.isFrozen
 
 actual fun <T> Observable<T>.toList(): Single<List<T>> =
     collect(ArrayList()) { list, item ->
-        val actualList = list.takeUnless(Any::isFrozen) ?: ArrayList<T>(list.size + 1).apply { addAll(list) }
-        actualList += item
-        actualList
+        val newList =
+            if (list.isFrozen) {
+                // We have to create a new ArrayList
+                ArrayList<T>(list.size + 1)
+                    .apply { addAll(list) }
+            } else {
+                list
+            }
+
+        newList.add(item)
+        newList
     }
