7c7e05229f4ccf4e3d96dc0a4dd82110c1087b92
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-10-16 15:57:46 +0100

    Optimized source builders

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Various.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Various.kt
@@ -1,5 +1,7 @@
 package com.badoo.reaktive.maybe
 
+import com.badoo.reaktive.disposable.Disposable
+
 inline fun <T> maybeUnsafe(crossinline onSubscribe: (observer: MaybeObserver<T>) -> Unit): Maybe<T> =
     object : Maybe<T> {
         override fun subscribe(observer: MaybeObserver<T>) {
@@ -8,34 +10,59 @@ inline fun <T> maybeUnsafe(crossinline onSubscribe: (observer: MaybeObserver<T>)
     }
 
 fun <T> maybeOf(value: T): Maybe<T> =
-    maybe { emitter ->
-        emitter.onSuccess(value)
+    maybeUnsafe { observer ->
+        val disposable = Disposable()
+        observer.onSubscribe(disposable)
+
+        if (!disposable.isDisposed) {
+            observer.onSuccess(value)
+        }
     }
 
 fun <T> T.toMaybe(): Maybe<T> = maybeOf(this)
 
 fun <T : Any> maybeOfNotNull(value: T?): Maybe<T> =
-    maybe { emitter ->
-        if (value == null) {
-            emitter.onComplete()
-        } else {
-            emitter.onSuccess(value)
+    maybeUnsafe { observer ->
+        val disposable = Disposable()
+        observer.onSubscribe(disposable)
+
+        if (!disposable.isDisposed) {
+            if (value == null) {
+                observer.onComplete()
+            } else {
+                observer.onSuccess(value)
+            }
         }
     }
 
 fun <T : Any> T?.toMaybeNotNull(): Maybe<T> = maybeOfNotNull(this)
 
 fun <T> maybeOfError(error: Throwable): Maybe<T> =
-    maybe { emitter ->
-        emitter.onError(error)
+    maybeUnsafe { observer ->
+        val disposable = Disposable()
+        observer.onSubscribe(disposable)
+
+        if (!disposable.isDisposed) {
+            observer.onError(error)
+        }
     }
 
 fun <T> Throwable.toMaybeOfError(): Maybe<T> = maybeOfError(this)
 
 fun <T> maybeOfEmpty(): Maybe<T> =
-    maybe(MaybeEmitter<*>::onComplete)
+    maybeUnsafe { observer ->
+        val disposable = Disposable()
+        observer.onSubscribe(disposable)
 
-fun <T> maybeOfNever(): Maybe<T> = maybe {}
+        if (!disposable.isDisposed) {
+            observer.onComplete()
+        }
+    }
+
+fun <T> maybeOfNever(): Maybe<T> =
+    maybeUnsafe { observer ->
+        observer.onSubscribe(Disposable())
+    }
 
 fun <T> maybeFromFunction(func: () -> T): Maybe<T> =
     maybe { emitter ->
