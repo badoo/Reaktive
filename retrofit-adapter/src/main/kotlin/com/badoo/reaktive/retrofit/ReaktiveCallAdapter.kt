682e5f8f0a2806d296fd5b9274991405d4090451
Alexey Korshun <lehakorshun@gmail.com>
2019-10-27 22:05:49 +0600

    use anonymous class for create call adapter

--- retrofit-adapter/src/main/kotlin/com/badoo/reaktive/retrofit/ReaktiveCallAdapter.kt
+++ retrofit-adapter/src/main/kotlin/com/badoo/reaktive/retrofit/ReaktiveCallAdapter.kt
@@ -1,37 +1,12 @@
 package com.badoo.reaktive.retrofit
 
-import com.badoo.reaktive.single.Single
-import com.badoo.reaktive.single.asCompletable
-import com.badoo.reaktive.single.asMaybe
-import com.badoo.reaktive.single.asObservable
-import com.badoo.reaktive.single.observeBody
 import retrofit2.Call
 import retrofit2.CallAdapter
 import java.lang.reflect.Type
 
-internal class ReaktiveCallAdapter(
-        private val responseType: Type,
-        private val isBody: Boolean,
-        private val isSingle: Boolean,
-        private val isMaybe: Boolean,
-        private val isCompletable: Boolean
-) : CallAdapter<Any, Any> {
+internal inline fun callAdapter(responseType: Type, crossinline adapt: (Call<Any>) -> Any): CallAdapter<Any, Any> =
+        object : CallAdapter<Any, Any> {
+            override fun responseType(): Type = responseType
 
-    override fun responseType(): Type {
-        return responseType
-    }
-
-    override fun adapt(call: Call<Any>): Any {
-        val responseSingle = call.asSingle()
-
-        val single: Single<*> = if (isBody) responseSingle.observeBody()
-        else responseSingle
-
-        return when {
-            isSingle -> single
-            isMaybe -> single.asMaybe()
-            isCompletable -> single.asCompletable()
-            else -> single.asObservable()
+            override fun adapt(call: Call<Any>): Any = adapt(call)
         }
-    }
-}
--- retrofit-adapter/src/main/kotlin/com/badoo/reaktive/retrofit/ReaktiveCallAdapterFactory.kt
+++ retrofit-adapter/src/main/kotlin/com/badoo/reaktive/retrofit/ReaktiveCallAdapterFactory.kt
@@ -4,6 +4,10 @@ import com.badoo.reaktive.completable.Completable
 import com.badoo.reaktive.maybe.Maybe
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.single.Single
+import com.badoo.reaktive.single.asCompletable
+import com.badoo.reaktive.single.asMaybe
+import com.badoo.reaktive.single.asObservable
+import com.badoo.reaktive.single.observeBody
 import retrofit2.CallAdapter
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -13,46 +17,46 @@ import java.lang.reflect.Type
 class ReaktiveCallAdapterFactory : CallAdapter.Factory() {
 
     override fun get(returnType: Type, annotations: Array<Annotation>, retrofit: Retrofit): CallAdapter<*, *>? {
+
         val rawType = getRawType(returnType)
+        val isBody = rawType.isBodyType()
 
-        if (rawType == Completable::class.java)
-            return ReaktiveCallAdapter(
-                    responseType = Unit::class.java,
-                    isBody = true,
-                    isSingle = false,
-                    isMaybe = false,
-                    isCompletable = true
-            )
-
-        val isSingle = rawType == Single::class.java
-        val isMaybe = rawType == Maybe::class.java
-
-        if (rawType != Observable::class.java && !isSingle && !isMaybe)
-            return null
-
-        var isBody = false
-        val responseType: Type
-        if (returnType !is ParameterizedType) {
-            val name = when {
-                isSingle -> "Single"
-                isMaybe -> "Maybe"
-                else -> "Observable"
-            }
-            throw IllegalStateException("$name return type must be parameterized as $name<Foo> or $name<? extends Foo>")
+        return when(rawType) {
+            Single::class.java -> callAdapter(returnType.getResponseType()) { call -> call.asSingle().transformToBody(isBody) }
+            Completable::class.java -> callAdapter(Unit::class.java) { call -> call.asSingle().observeBody().asCompletable() }
+            Maybe::class.java -> callAdapter(returnType.getResponseType()) { call -> call.asSingle().transformToBody(isBody).asMaybe() }
+            Observable::class.java -> callAdapter(returnType.getResponseType()) { call -> call.asSingle().transformToBody(isBody).asObservable() }
+            else -> return null
         }
+    }
 
-        val observableType = getParameterUpperBound(0, returnType)
-        when (getRawType(observableType)) {
+    private fun Type.getResponseType(): Type {
+        check(this is ParameterizedType) { "Return type must be parameterized" }
+
+        val observableType = getParameterUpperBound(0, this)
+        return when (getRawType(observableType)) {
             Response::class.java -> {
                 check(observableType is ParameterizedType) { "Response must be parameterized as Response<Foo> or Response<? extends Foo>" }
-                responseType = getParameterUpperBound(0, observableType)
+                getParameterUpperBound(0, observableType)
             }
+            else -> observableType
+        }
+    }
+
+    private fun Type.isBodyType(): Boolean {
+        return when(this) {
+            Completable::class.java -> true
             else -> {
-                responseType = observableType
-                isBody = true
+                check(this is ParameterizedType) { "Return type must be parameterized" }
+                getRawType(getParameterUpperBound(0, this)) != Response::class.java
             }
         }
+    }
+}
 
-        return ReaktiveCallAdapter(responseType, isBody, isSingle, isMaybe, false)
+private fun <T> Single<Response<T>>.transformToBody(transform: Boolean): Single<*> {
+    return when(transform) {
+        true -> this.observeBody()
+        else -> this
     }
 }
