272b3e046d5a76c0c266256ec81f25094b714d55
Mike <mikenjordan93@gmail.com>
2019-12-13 16:12:22 +0000

    Remove triple implementation due to conflicting signature.

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/ZipWith.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/ZipWith.kt
@@ -2,6 +2,3 @@ package com.badoo.reaktive.maybe
 
 fun <T, R> Maybe<T>.zipWith(maybe: Maybe<R>): Maybe<Pair<T, R>> =
     zip(this, maybe) { first, second -> first to second }
-
-fun <T, R, I> Maybe<Pair<T, R>>.zipWith(maybe: Maybe<I>): Maybe<Triple<T, R, I>> =
-    zip(this, maybe) { pair, other -> Triple(pair.first, pair.second, other) }
