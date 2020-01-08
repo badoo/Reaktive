cbd8c9aa7321d73273753318ffa474f18b885a36
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2020-01-08 19:48:58 +0000

    Remove useless @Suppress annotation

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/annotations/Annotations.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/annotations/Annotations.kt
@@ -1,6 +1,5 @@
 package com.badoo.reaktive.annotations
 
-@Suppress("kotlin.Experimental")
 @Experimental(Experimental.Level.WARNING)
 @Retention(AnnotationRetention.BINARY)
 annotation class ExperimentalReaktiveApi
