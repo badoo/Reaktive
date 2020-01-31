407d4b6a69ef754470d2eeaf256b3a1281e8e307
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2020-01-31 15:49:58 +0000

    Added wrappers for Swift

---
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/maybe/Wrapper.kt
@@ -0,0 +1,33 @@
+package com.badoo.reaktive.maybe
+
+import com.badoo.reaktive.annotations.UseReturnValue
+import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.maybe.subscribe as subscribeRx
+
+/**
+ * Wrappers are normally exposed to Swift.
+ * You might want to enable Objective-C generics,
+ * please refer to the [documentation][https://kotlinlang.org/docs/reference/native/objc_interop.html#to-use]
+ * for more information.
+ * You can also extend the wrapper class if you need to expose any additional operators.
+ */
+open class MaybeWrapper<out T>(inner: Maybe<T>) : Maybe<T> by inner {
+
+    @UseReturnValue
+    fun <T> Maybe<T>.subscribe(
+        isThreadLocal: Boolean = false,
+        onSubscribe: ((Disposable) -> Unit)? = null,
+        onError: ((Throwable) -> Unit)? = null,
+        onComplete: (() -> Unit)? = null,
+        onSuccess: ((T) -> Unit)? = null
+    ): Disposable =
+        subscribeRx(
+            isThreadLocal = isThreadLocal,
+            onSubscribe = onSubscribe,
+            onError = onError,
+            onComplete = onComplete,
+            onSuccess = onSuccess
+        )
+}
+
+fun <T> Maybe<T>.wrap(): MaybeWrapper<T> = MaybeWrapper(this)
---
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/Wrapper.kt
@@ -0,0 +1,33 @@
+package com.badoo.reaktive.observable
+
+import com.badoo.reaktive.annotations.UseReturnValue
+import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.observable.subscribe as subscribeRx
+
+/**
+ * Wrappers are normally exposed to Swift.
+ * You might want to enable Objective-C generics,
+ * please refer to the [documentation][https://kotlinlang.org/docs/reference/native/objc_interop.html#to-use]
+ * for more information.
+ * You can also extend the wrapper class if you need to expose any additional operators.
+ */
+open class ObservableWrapper<out T>(inner: Observable<T>) : Observable<T> by inner {
+
+    @UseReturnValue
+    fun <T> Observable<T>.subscribe(
+        isThreadLocal: Boolean = false,
+        onSubscribe: ((Disposable) -> Unit)? = null,
+        onError: ((Throwable) -> Unit)? = null,
+        onComplete: (() -> Unit)? = null,
+        onNext: ((T) -> Unit)? = null
+    ): Disposable =
+        subscribeRx(
+            isThreadLocal = isThreadLocal,
+            onSubscribe = onSubscribe,
+            onError = onError,
+            onComplete = onComplete,
+            onNext = onNext
+        )
+}
+
+fun <T> Observable<T>.wrap(): ObservableWrapper<T> = ObservableWrapper(this)
