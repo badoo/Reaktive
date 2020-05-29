b15b32c598152df64b032ef829eeb8fd4cf54b93
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2020-05-29 17:28:03 +0100

    Print stack trace in assertNotError

--- reaktive-testing/src/commonMain/kotlin/com/badoo/reaktive/test/base/TestObserverExt.kt
+++ reaktive-testing/src/commonMain/kotlin/com/badoo/reaktive/test/base/TestObserverExt.kt
@@ -1,27 +1,50 @@
 package com.badoo.reaktive.test.base
 
+import com.badoo.reaktive.utils.printStack
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
 fun TestObserver.assertError() {
     assertTrue(isError, "Source did not fail")
 }
 
 fun TestObserver.assertError(expectedError: Throwable) {
-    assertEquals(expectedError, error, "Source error does not match")
+    val error = error
+    if (error == null) {
+        fail("Source did not fail")
+    } else {
+        try {
+            assertEquals(expectedError, error, "Source error does not match, the actual error is printed above")
+        } catch (e: AssertionError) {
+            error.printStack()
+            throw e
+        }
+    }
 }
 
 fun TestObserver.assertError(predicate: (Throwable) -> Boolean) {
-    assertError()
-    val error = this.error!!
-    assertTrue(predicate(error), "Source error does not match the predicate: $error")
+    val error = error
+    if (error == null) {
+        fail("Source did not fail")
+    } else {
+        try {
+            assertTrue(predicate(error), "Source error does not match the predicate, the actual error is printed above")
+        } catch (e: AssertionError) {
+            error.printStack()
+            throw e
+        }
+    }
 }
 
 fun TestObserver.assertNotError() {
-    assertFalse(isError, "Source failed")
+    error?.also {
+        it.printStack()
+        fail("Source failed, the actual error is printed above")
+    }
 }
 
 fun TestObserver.assertSubscribed() {
