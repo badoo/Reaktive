b5196a2ddc6a48e3f8507ad5aaefa01e166d3d13
Mina Eweida <mina.eweida@careem.com>
2020-06-17 18:51:14 +0400

    Another fix
    There are 2 cases using `window` will crash:
    1- ReferenceError: window is not defined
    2- TypeError: window.setTimeout is not a function (or setInterval or clearTimeout or clearInterval)
    
    So for instance if someone works around window is not defined by doing the following:
    `global.window = {document: {createElementNS: () => {return {}} }};`
    
    If we still don't catch this error, MainScheduler will break.
    
    So this approach will ensure that if window fails, we will fallback to external functions which should map to nodeJs timer functions

--- utils/src/jsMain/kotlin/com/badoo/reaktive/utils/Timers.kt
+++ utils/src/jsMain/kotlin/com/badoo/reaktive/utils/Timers.kt
@@ -7,23 +7,36 @@ external fun setInterval(handler: dynamic, timeout: Int = definedExternally, var
 external fun clearTimeout(handle: Int = definedExternally)
 external fun clearInterval(handle: Int = definedExternally)
 
-
 object Timers {
-    // TODO Verbose, just using for POC, remove
-    private val isWindowDefined: Boolean = jsTypeOf(window) != "undefined"
+    fun setTimeout(delayMillis: Long, task: () -> Unit, vararg arguments: Any?): Int {
+        return try {
+            window.setTimeout(task, delayMillis.toInt(), arguments)
+        } catch (ex: dynamic) {
+            setTimeout(task, delayMillis.toInt(), arguments)
+        }
+    }
 
-    fun setTimeout(delayMillis: Long, task: () -> Unit, vararg arguments: Any?): Int =
-        if (isWindowDefined) window.setTimeout(task, delayMillis.toInt(), arguments) else setTimeout(task, delayMillis.toInt(), arguments)
-
-    fun setInterval(delayMillis: Long, task: () -> Unit, vararg arguments: Any?): Int =
-        if (isWindowDefined) window.setInterval(task, delayMillis.toInt(), arguments) else setInterval(task, delayMillis.toInt(), arguments)
+    fun setInterval(delayMillis: Long, task: () -> Unit, vararg arguments: Any?): Int {
+        return try {
+            window.setInterval(task, delayMillis.toInt(), arguments)
+        } catch (ex: dynamic) {
+            setInterval(task, delayMillis.toInt(), arguments)
+        }
+    }
 
     fun clearTimeout(interval: Long) {
-        if (isWindowDefined) window.clearTimeout(interval.toInt()) else clearTimeout(interval.toInt())
+        try {
+            window.clearTimeout(interval.toInt())
+        } catch (ex: dynamic) {
+            clearTimeout(interval.toInt())
+        }
     }
 
     fun clearInterval(interval: Long) {
-        if (isWindowDefined) window.clearInterval(interval.toInt()) else clearInterval(interval.toInt())
+        try {
+            window.clearInterval(interval.toInt())
+        } catch (ex: dynamic) {
+            clearInterval(interval.toInt())
+        }
     }
-
 }
