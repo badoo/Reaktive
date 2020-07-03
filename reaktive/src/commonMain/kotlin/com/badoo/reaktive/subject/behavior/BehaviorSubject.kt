ce7e111ead11555213c4dda1aa6d51310b0acaf6
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2020-07-03 14:22:37 +0100

    Add docs for interfaces with factory functions

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/subject/behavior/BehaviorSubject.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/subject/behavior/BehaviorSubject.kt
@@ -2,4 +2,8 @@ package com.badoo.reaktive.subject.behavior
 
 import com.badoo.reaktive.subject.Subject
 
+/**
+ * The following factory function is available:
+ * - `BehaviorSubject<T>(initialValue: T)`
+ */
 interface BehaviorSubject<T> : Subject<T>, BehaviorRelay<T>
