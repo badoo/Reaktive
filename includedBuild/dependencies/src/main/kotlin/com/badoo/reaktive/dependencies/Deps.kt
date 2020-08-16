c0048c92cbb06780a62a26d9e222c77b39115f99
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2020-08-16 18:57:05 +0100

    Update Kotlin to 1.4.0

--- includedBuild/dependencies/src/main/kotlin/com/badoo/reaktive/dependencies/Deps.kt
+++ includedBuild/dependencies/src/main/kotlin/com/badoo/reaktive/dependencies/Deps.kt
@@ -2,8 +2,8 @@ package com.badoo.reaktive.dependencies
 
 object Deps {
 
-    private const val kotlinVersion = "1.4.0-rc"
-    private const val coroutinesVersion = "1.3.8-1.4.0-rc"
+    private const val kotlinVersion = "1.4.0"
+    private const val coroutinesVersion = "1.3.9"
     private const val detektVersion = "1.9.1"
     private const val asmVersion = "6.0"
 
