692cdad321116a5b676388cd980a636c54fea1a3
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2020-10-09 13:42:32 +0100

    Add test assertions chaining

--- reaktive-testing/src/commonMain/kotlin/com/badoo/reaktive/test/observable/TestObservableObserverExt.kt
+++ reaktive-testing/src/commonMain/kotlin/com/badoo/reaktive/test/observable/TestObservableObserverExt.kt
@@ -6,28 +6,40 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-fun <T> TestObservableObserver<T>.assertValues(expectedValues: List<T>) {
+fun <T> TestObservableObserver<T>.assertValues(expectedValues: List<T>): TestObservableObserver<T> {
     assertEquals(expectedValues, values, "Observable values do not match")
+
+    return this
 }
 
-fun <T> TestObservableObserver<T>.assertValues(vararg expectedValues: T) {
+fun <T> TestObservableObserver<T>.assertValues(vararg expectedValues: T): TestObservableObserver<T> {
     assertValues(listOf(*expectedValues))
+
+    return this
 }
 
-fun <T> TestObservableObserver<T>.assertValue(expectedValue: T) {
+fun <T> TestObservableObserver<T>.assertValue(expectedValue: T): TestObservableObserver<T> {
     assertValues(listOf(expectedValue))
+
+    return this
 }
 
-fun TestObservableObserver<*>.assertNoValues() {
+fun <T> TestObservableObserver<T>.assertNoValues(): TestObservableObserver<T> {
     assertValues(emptyList())
+
+    return this
 }
 
-fun TestObservableObserver<*>.assertComplete() {
+fun <T> TestObservableObserver<T>.assertComplete(): TestObservableObserver<T> {
     assertTrue(isComplete, "Observable did not complete")
+
+    return this
 }
 
-fun TestObservableObserver<*>.assertNotComplete() {
+fun <T> TestObservableObserver<T>.assertNotComplete(): TestObservableObserver<T> {
     assertFalse(isComplete, "Observable completed")
+
+    return this
 }
 
 fun <T> Observable<T>.test(autoFreeze: Boolean = true): TestObservableObserver<T> {
