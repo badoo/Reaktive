8a109ce32d3a1b3f5b888284c495d2de60cb1ac4
Arkadii Ivanov <arkann1985@gmail.com>
2021-02-19 16:58:32 +0000

    KDocs for Single (part 1)

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/FlatMap.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/FlatMap.kt
@@ -5,6 +5,12 @@ import com.badoo.reaktive.base.Observer
 import com.badoo.reaktive.base.tryCatch
 import com.badoo.reaktive.disposable.Disposable
 
+/**
+ * Calls the `mapper` with the value emitted by the [Single] and subscribes to the returned inner [Single].
+ * Emits the value from the inner [Single].
+ *
+ * Please refer to the corresponding RxJava [document](http://reactivex.io/RxJava/javadoc/io/reactivex/Single.html#flatMap-io.reactivex.functions.Function-).
+ */
 fun <T, R> Single<T>.flatMap(mapper: (T) -> Single<R>): Single<R> =
     single { emitter ->
         subscribe(
@@ -25,6 +31,12 @@ fun <T, R> Single<T>.flatMap(mapper: (T) -> Single<R>): Single<R> =
         )
     }
 
+/**
+ * Calls the `mapper` with the value emitted by the [Single] and subscribes to the returned inner [Single].
+ * When the inner [Single] emits, calls the `resultSelector` function with the original and the inner values and emits the result.
+ *
+ * Please refer to the corresponding RxJava [document](http://reactivex.io/RxJava/javadoc/io/reactivex/Single.html#flatMap-io.reactivex.functions.Function-).
+ */
 fun <T, U, R> Single<T>.flatMap(mapper: (T) -> Single<U>, resultSelector: (T, U) -> R): Single<R> =
     flatMap { t ->
         mapper(t).map { u -> resultSelector(t, u) }
--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Merge.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Merge.kt
@@ -5,10 +5,20 @@ import com.badoo.reaktive.observable.asObservable
 import com.badoo.reaktive.observable.flatMapSingle
 import com.badoo.reaktive.observable.merge
 
+/**
+ * Merges multiple [Single]s into one [Observable], running all [Single]s simultaneously.
+ *
+ * Please refer to the corresponding RxJava [document](http://reactivex.io/RxJava/javadoc/io/reactivex/Single.html#merge-java.lang.Iterable-).
+ */
 fun <T> Iterable<Single<T>>.merge(): Observable<T> =
     asObservable()
         .flatMapSingle { it }
 
+/**
+ * Merges multiple [Single]s into one [Observable], running all [Single]s simultaneously.
+ *
+ * Please refer to the corresponding RxJava [document](http://reactivex.io/RxJava/javadoc/io/reactivex/Single.html#merge-java.lang.Iterable-).
+ */
 fun <T> merge(vararg sources: Single<T>): Observable<T> =
     sources
         .map(Single<T>::asObservable)
