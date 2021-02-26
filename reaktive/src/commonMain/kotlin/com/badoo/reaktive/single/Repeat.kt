2c567da083c6fcea7a0b011adcbb6829cc53b76b
Arkadii Ivanov <arkann1985@gmail.com>
2021-02-26 15:05:55 +0000

    KDocs for Single (part 2)

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Repeat.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/Repeat.kt
@@ -3,4 +3,9 @@ package com.badoo.reaktive.single
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.repeat
 
+/**
+ * When the [Single] signals `onSuccess`, re-subscribes to the [Single], `count` times.
+ *
+ * Please refer to the corresponding RxJava [document](http://reactivex.io/RxJava/javadoc/io/reactivex/Single.html#repeat-long-).
+ */
 fun <T> Single<T>.repeat(count: Int = -1): Observable<T> = asObservable().repeat(count = count)
--- reaktive/src/jvmCommonMain/kotlin/com/badoo/reaktive/single/SingleByEmitter.kt
+++ reaktive/src/jvmCommonMain/kotlin/com/badoo/reaktive/single/SingleByEmitter.kt
@@ -4,6 +4,11 @@ import com.badoo.reaktive.base.tryCatch
 import com.badoo.reaktive.disposable.Disposable
 import com.badoo.reaktive.disposable.DisposableWrapper
 
+/**
+ * Creates a [Single] with manual signalling via [SingleEmitter].
+ *
+ * Please refer to the corresponding RxJava [document](http://reactivex.io/RxJava/javadoc/io/reactivex/Single.html#create-io.reactivex.SingleOnSubscribe-).
+ */
 actual inline fun <T> single(crossinline onSubscribe: (emitter: SingleEmitter<T>) -> Unit): Single<T> =
     singleUnsafe { observer ->
         val emitter =
