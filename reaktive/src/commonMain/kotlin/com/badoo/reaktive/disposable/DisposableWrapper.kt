d7947b1a598a936dde2e217603b5208d7cb8e401
Arkadii Ivanov <arkann1985@gmail.com>
2021-05-08 17:04:03 +0100

    Make DisposableWrapper to extend SerialDisposable

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/disposable/DisposableWrapper.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/disposable/DisposableWrapper.kt
@@ -4,33 +4,4 @@ package com.badoo.reaktive.disposable
  * Thread-safe container of one [Disposable]
  */
 @Deprecated("Please use SerialDisposable", ReplaceWith("SerialDisposable"))
-@Suppress("EmptyDefaultConstructor")
-expect open class DisposableWrapper() : Disposable {
-
-    override val isDisposed: Boolean
-
-    /**
-     * Disposes this [DisposableWrapper] and a stored [Disposable] if any.
-     * Any future [Disposable] will be immediately disposed.
-     */
-    override fun dispose()
-
-    /**
-     * Atomically either replaces any existing [Disposable]
-     * with the specified one or disposes it if wrapper is already disposed.
-     * Also disposes any replaced [Disposable].
-     *
-     * @param disposable a new [Disposable], will be disposed if wrapper is already dispose
-     */
-    fun set(disposable: Disposable?)
-
-    /**
-     * Atomically either replaces any existing [Disposable]
-     * with the specified one or disposes it if wrapper is already disposed.
-     * Does not dispose any replaced [Disposable].
-     *
-     * @param disposable a new [Disposable], will be disposed if wrapper is already dispose
-     * @return replaced [Disposable] if any
-     */
-    fun replace(disposable: Disposable?): Disposable?
-}
+open class DisposableWrapper : SerialDisposable()
