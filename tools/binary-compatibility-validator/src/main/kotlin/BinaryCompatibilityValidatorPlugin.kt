e2d8047dc473a3391a0c759c88f7c22147f3c9bb
CherryPerry <ya.cherryperry@yandex.ru>
2021-05-14 13:40:56 +0100

    Add prominent notice of modification

--- tools/binary-compatibility-validator/src/main/kotlin/BinaryCompatibilityValidatorPlugin.kt
+++ tools/binary-compatibility-validator/src/main/kotlin/BinaryCompatibilityValidatorPlugin.kt
@@ -1,6 +1,8 @@
 /*
  * Copyright 2016-2020 JetBrains s.r.o.
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ *
+ * This file has been modified by Badoo to add support Kotlin 1.5.0.
  */
 
 package kotlinx.validation
