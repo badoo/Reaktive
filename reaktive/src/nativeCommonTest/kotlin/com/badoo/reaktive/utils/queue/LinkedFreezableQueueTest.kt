43852a75e306ed31ae4c0bd43d10acde66126e27
Arkadii Ivanov <arkann1985@gmail.com>
2023-03-02 20:46:06 +0000

    Removed LinkedFreezableQueue and optimized BufferedExecutor

--- reaktive/src/nativeCommonTest/kotlin/com/badoo/reaktive/utils/queue/LinkedFreezableQueueTest.kt
+++
@@ -1,5 +0,0 @@
-package com.badoo.reaktive.utils.queue
-
-import kotlin.native.concurrent.freeze
-
-class LinkedFreezableQueueTest : QueueTests by QueueTestsImpl(LinkedFreezableQueue<String?>().freeze())
